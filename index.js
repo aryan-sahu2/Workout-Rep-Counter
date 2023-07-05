a86184041935e1503e4aa83253a52c71b4d9be37
aryan-sahu2 <138488610+aryan-sahu2@users.noreply.github.com>
2023-07-05 11:43:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function increment(){
     count+=1
     countEl.textContent=count
 }
+function decrement(){
+    count-=1
+    countEl.textContent=count
+}
 
 function save(){
     let result=+count+" - "
