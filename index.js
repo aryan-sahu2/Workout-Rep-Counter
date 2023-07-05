23da45b283a82eb4d8aec0bc4ac5cf56d806c33e
aryan-sahu2 <138488610+aryan-sahu2@users.noreply.github.com>
2023-07-05 08:26:48 +0530

    Add counter app files via upload
    Modifications shall be made and uploaded as and when it's done.

---
+++ index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count").innerText=5
+
+let countEl=document.getElementById("count_el")   //getElementById is a function in which we are passing arguments 
+let count=parseInt(countEl.innerText)              //things written before '.' are mostly the Objects on which we're performing functions
+let saveEl= document.getElementById("save-el")
+
+function increment(){
+    count+=1
+    countEl.textContent=count
+}
+
+function save(){
+    let result=+count+" - "
+    saveEl.textContent+=result
+
+    countEl.textContent=0
+    count=0
+}
